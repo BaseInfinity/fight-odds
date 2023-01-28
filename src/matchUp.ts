0a8a5b24da60a22fe415a2e9b440fcf0a52d9e77
Stefan Ayala <stefanayala3266@gmail.com>
2023-01-28 12:45:10 -0800

    show prob of winning first

--- src/matchUp.ts
+++ src/matchUp.ts
@@ -10,7 +10,7 @@ export default class MatchUp {
    }
 
    getSummary() {
-       return `\`\`\`${this.home.fullName}: ${this.home.getAmericanOdds()} | ${Number(this.home.decimalOdds).toFixed(2)} | ${this.home.getBritishOdds()} | ${this.home.getWinProbability()} | ${this.home.getReturnWinnings(100)}
-${this.away.fullName}: ${this.away.getAmericanOdds()} | ${Number(this.away.decimalOdds).toFixed(2)} | ${this.away.getBritishOdds()} | ${this.away.getWinProbability()} | ${this.away.getReturnWinnings(100)}\`\`\``
+       return `\`\`\`${this.home.fullName}: ${this.home.getWinProbability()} | ${this.home.getAmericanOdds()} | ${Number(this.home.decimalOdds).toFixed(2)} | ${this.home.getBritishOdds()} | ${this.home.getReturnWinnings(100)}
+${this.away.fullName}: ${this.away.getWinProbability()} | ${this.away.getAmericanOdds()} | ${Number(this.away.decimalOdds).toFixed(2)} | ${this.away.getBritishOdds()} | ${this.away.getWinProbability()} | ${this.away.getReturnWinnings(100)}\`\`\``
    }
 }
